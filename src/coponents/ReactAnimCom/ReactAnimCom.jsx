a6a143c406f33585c21c6d4d41dcd8bd295927ed
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-23 01:27:35 +0600

    add deffrent animation

--- src/coponents/ReactAnimCom/ReactAnimCom.jsx
+++ src/coponents/ReactAnimCom/ReactAnimCom.jsx
@@ -2,6 +2,7 @@
 
 import {
   AttentionSeeker,
+  Fade,
   // import {
   //   AttentionSeeker,
   //   Bounce,
@@ -87,13 +88,33 @@ const ReactAnimCom = () => {
   const handleMouseEnter = hoverChr => {
     let elem = `.${hoverChr}`;
     let animChr = document.querySelector(elem);
-    if (animChr.classList.length === 5) {
-      console.log("not have");
-      animChr.classList.add("animate__rubberBand");
-    } else {
-      console.log("have");
+    animChr.classList.add("animate__rubberBand");
+    setTimeout(() => {
       animChr.classList.remove("animate__rubberBand");
-    }
+    }, 1000);
+    // if (animChr.classList.length === 5) {
+    //   console.log("not have");
+    //   animChr.classList.add("animate__rubberBand");
+    // } else {
+    //   console.log("have");
+    //   animChr.classList.remove("animate__rubberBand");
+    // }
+    console.log(animChr.classList.length);
+  };
+
+  const handleMouseLeave = hoverChr => {
+    let elem = `.${hoverChr}`;
+    let animChr = document.querySelector(elem);
+    // animChr.classList.remove("animate__rubberBand");
+    // animChr.classList.add("animate__shakeX");
+    // animChr.setAttribute("bounce", false);
+    // if (animChr.classList.length === 5) {
+    //   console.log("not have");
+    //   animChr.classList.add("animate__rubberBand");
+    // } else {
+    //   console.log("have");
+    //   animChr.classList.remove("animate__rubberBand");
+    // }
     console.log(animChr.classList.length);
   };
 
@@ -101,7 +122,7 @@ const ReactAnimCom = () => {
     <div>
       <div className="w-[600px] h-[600px] flex items-center justify-center mx-auto ">
         {/* <Bounce direction="ri">
-          <h1 className="text-5xl font-medium">React Reveal</h1>
+          <h1 className="text-9xl font-medium">React Reveal</h1>
         </Bounce> */}
         <div
           // className="animate__rubberBand px-1"
@@ -123,41 +144,102 @@ const ReactAnimCom = () => {
           // tada
           // wobble
         >
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr1 ">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr1")}>R</p>
+          <AttentionSeeker
+            bounce={false}
+            className="text-9xl font-bold tracking-tighter leading-3 anim-chr1 "
+          >
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr1")}
+              onMouseLeave={() => handleMouseLeave("anim-chr1")}
+            >
+              R
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr2">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr2")}>e</p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr2">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr2")}
+              onMouseLeave={() => handleMouseLeave("anim-chr2")}
+            >
+              e
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr3">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr3")}>a</p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr3">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr3")}
+              onMouseLeave={() => handleMouseLeave("anim-chr3")}
+            >
+              a
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr4">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr4")}>c</p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr4">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr4")}
+              onMouseLeave={() => handleMouseLeave("anim-chr4")}
+            >
+              c
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr5">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr5")}>t</p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr5">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr5")}
+              onMouseLeave={() => handleMouseLeave("anim-chr5")}
+            >
+              t
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr6">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr6")}></p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr6">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr6")}
+              onMouseLeave={() => handleMouseLeave("anim-chr6")}
+            ></p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr7">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr7")}>R</p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr7">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr7")}
+              onMouseLeave={() => handleMouseLeave("anim-chr7")}
+            >
+              R
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl  font-bold tracking-tight anim-chr8">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr8")}>e</p>
+          <AttentionSeeker className="text-9xl  font-bold tracking-tighter leading-3 anim-chr8">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr8")}
+              onMouseLeave={() => handleMouseLeave("anim-chr8")}
+            >
+              e
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr9">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr9")}>v</p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr9">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr9")}
+              onMouseLeave={() => handleMouseLeave("anim-chr9")}
+            >
+              v
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr10">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr10")}>e</p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr10">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr10")}
+              onMouseLeave={() => handleMouseLeave("anim-chr10")}
+            >
+              e
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr11">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr11")}>a</p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr11">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr11")}
+              onMouseLeave={() => handleMouseLeave("anim-chr11")}
+            >
+              a
+            </p>
           </AttentionSeeker>
-          <AttentionSeeker className="text-5xl font-bold tracking-tight anim-chr12">
-            <p onMouseEnter={() => handleMouseEnter("anim-chr12")}>l</p>
+          <AttentionSeeker className="text-9xl font-bold tracking-tighter leading-3 anim-chr12">
+            <p
+              onMouseEnter={() => handleMouseEnter("anim-chr12")}
+              onMouseLeave={() => handleMouseLeave("anim-chr12")}
+            >
+              l
+            </p>
           </AttentionSeeker>
         </div>
       </div>
